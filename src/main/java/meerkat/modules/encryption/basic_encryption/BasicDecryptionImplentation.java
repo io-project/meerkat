529ab0c21d870438e4af7c6acf4c41d9b777445f
Karol <karol.banys@gmail.com>
2013-11-05 17:41:13 +0100

    Add TODO

--- src/main/java/meerkat/modules/encryption/basic_encryption/BasicDecryptionImplentation.java
+++ src/main/java/meerkat/modules/encryption/basic_encryption/BasicDecryptionImplentation.java
@@ -3,5 +3,5 @@ package meerkat.modules.encryption.basic_encryption;
 import meerkat.modules.encryption.IDecryptionImplementation;
 
 public class BasicDecryptionImplentation implements IDecryptionImplementation{
-
+	//TODO implement 
 }
