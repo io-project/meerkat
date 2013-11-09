95774e9cc219cf373b4226b60eb2329a5686c14f
Maciej Poleski <to.java.project.d8fj39saldkj28@gmail.com>
2013-11-09 13:35:30 +0100

    Interface for DialogValidator

--- src/main/java/meerkat/modules/gui/IDialogValidator.java
+++ src/main/java/meerkat/modules/gui/IDialogValidator.java
@@ -1,5 +1,7 @@
 package meerkat.modules.gui;
 
+import java.util.Map;
+
 /**
  * Validator służący do sprawdzenia wszystkich danych wprowadzonych przez użytkownika do okna dialogowego.
  * Używany po validacji poszczególnych pól dedykowanymi validatorami.
@@ -7,4 +9,13 @@ package meerkat.modules.gui;
  * @author Maciej Poleski
  */
 public interface IDialogValidator {
+    /**
+     * Sprawdza czy wartość wszystkich pól jest akceptowalna. Można założyć że validatory poszczególnych pól zostały
+     * uruchomione i wszystkie zaakceptowały.
+     *
+     * @param fields Mapa pól. Kluczem jest etykieta. Wartością jest obiekt typu powiązanego z danym polem tak jak w
+     *               validatorach poszczególnych pól.
+     * @return true - jest ok, false w przeciwnym wypadku.
+     */
+    boolean validate(Map<String, ?> fields);    // Jeżeli ktoś ma lepszy pomysł - jestem otwarty na sugestie
 }
