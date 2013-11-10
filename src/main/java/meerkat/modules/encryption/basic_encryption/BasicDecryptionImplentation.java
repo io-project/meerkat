865999806661035641d029eb8e8caf8bc965e71d
Karol <karol.banys@gmail.com>
2013-11-10 12:42:49 +0100

    Remove Basic implementation.

--- src/main/java/meerkat/modules/encryption/basic_encryption/BasicDecryptionImplentation.java
+++
@@ -1,7 +0,0 @@
-package meerkat.modules.encryption.basic_encryption;
-
-import meerkat.modules.encryption.IDecryptionImplementation;
-
-public class BasicDecryptionImplentation implements IDecryptionImplementation{
-	//TODO implement 
-}
