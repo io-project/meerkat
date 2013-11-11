b509db1eea33f17c095b4d33efd3e1b76993fa5b
Maciej Poleski <to.java.project.d8fj39saldkj28@gmail.com>
2013-11-11 19:13:11 +0100

    All already shipped plugins are registered in core.

--- src/main/java/meerkat/modules/core/App.java
+++ src/main/java/meerkat/modules/core/App.java
@@ -1,5 +1,9 @@
 package meerkat.modules.core;
 
+import meerkat.modules.encryption.xor.XorPlugin;
+import meerkat.modules.import_export.basic.BasicPlugin;
+import meerkat.modules.serialization.simpleSerialization.SimpleSerializationPlugin;
+
 /**
  * Ta klasa służy do uruchomienia aplikacji.
  *
@@ -9,6 +13,9 @@ public class App {
     private static void registerAllPlugins(Core core) {
         // Tutaj należy rejestrować pluginy
         // core.registerPlugin(...);
+        core.registerPlugin(new XorPlugin());
+        core.registerPlugin(new BasicPlugin());
+        core.registerPlugin(new SimpleSerializationPlugin());
     }
 
     public static void main(String[] args) {
