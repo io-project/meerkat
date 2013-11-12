e957dbcdce47ab870d56f4a8c7fcbdb092c79066
Maciej Poleski <to.java.project.d8fj39saldkj28@gmail.com>
2013-11-12 19:24:25 +0100

    XOR encryption test stub.

---
+++ src/test/java/meerkat/modules/encryption/xor/XorEncryptionTest.java
@@ -0,0 +1,17 @@
+package meerkat.modules.encryption.xor;
+
+import org.junit.After;
+import org.junit.Before;
+
+
+public class XorEncryptionTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
