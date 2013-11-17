2911a0986c5cd789f7296a17292d3d67cdbe0658
Karol <karol.banys@gmail.com>
2013-11-17 17:46:55 +0100

    Add XorDecryptionTest and XorEncryptionTest both pass.

--- src/test/java/meerkat/modules/encryption/xor/XorDecryptionTest.java
+++ src/test/java/meerkat/modules/encryption/xor/XorDecryptionTest.java
@@ -1,14 +1,74 @@
 package meerkat.modules.encryption.xor;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Random;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 
 public class XorDecryptionTest {
-    @Before
+	MockReadableChannel readChannel = null;
+	MockWriteableChannel writeChannel = null;
+	XorDecryption xorDecryption = null;
+	
+	@Before
     public void setUp() throws Exception {
-
+		readChannel = new MockReadableChannel();
+		writeChannel = new MockWriteableChannel();
+		xorDecryption = new XorDecryption();
+		xorDecryption.setInputChannel(readChannel);
+		xorDecryption.setOutputChannel(writeChannel);
+		char[] password = {'a','b'};
+		xorDecryption.getXorAddition().makeHashArrayFromPassword(password);
     }
+    
+	@Test
+	public void checkSimpleXorEncoding() throws Exception{
+		/*preprare*/
+		int size = 64;
+		byte[] in = new byte[size];
+		new Random().nextBytes(in);
+		writeChannel.setSize(size);
+		writeChannel.reset();
+		
+		/*action*/
+		readChannel.setSendMessage(in);
+		xorDecryption.run();
+		readChannel.setSendMessage(writeChannel.getGotMessage());
+		writeChannel.reset();
+		xorDecryption.run();
+		
+		/*assertion*/
+		int bytesRead = writeChannel.getGotLength();
+		assertEquals(size, bytesRead);
+		assertArrayEquals(in, writeChannel.getGotMessage());
+	}
+	
+	@Test
+	public void checkMultiBufferXorEncoding() throws Exception{
+		/*preprare*/
+		int size = 1500;
+		byte[] in = new byte[size];
+		new Random().nextBytes(in);
+		writeChannel.setSize(size);
+		writeChannel.reset();
+		
+		/*action*/
+		readChannel.setSendMessage(in);
+		xorDecryption.run();
+		readChannel.setSendMessage(writeChannel.getGotMessage());
+		writeChannel.reset();
+		xorDecryption.run();
+		
+		/*assertion*/
+		int bytesRead = writeChannel.getGotLength();
+		assertEquals(size, bytesRead);
+		assertArrayEquals(in, writeChannel.getGotMessage());
+	}
 
     @After
     public void tearDown() throws Exception {
--- src/test/java/meerkat/modules/encryption/xor/XorEncryptionTest.java
+++ src/test/java/meerkat/modules/encryption/xor/XorEncryptionTest.java
@@ -1,14 +1,77 @@
 package meerkat.modules.encryption.xor;
 
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import meerkat.modules.encryption.xor.MockReadableChannel;
+import meerkat.modules.encryption.xor.MockWriteableChannel;
+import meerkat.modules.encryption.xor.XorEncryption;
+
 import org.junit.After;
 import org.junit.Before;
-
+import org.junit.Test;
 
 public class XorEncryptionTest {
-    @Before
+    
+	MockReadableChannel readChannel = null;
+	MockWriteableChannel writeChannel = null;
+	XorEncryption xorEncryption = null;
+	
+	@Before
     public void setUp() throws Exception {
-
+		readChannel = new MockReadableChannel();
+		writeChannel = new MockWriteableChannel();
+		xorEncryption = new XorEncryption();
+		xorEncryption.setInputChannel(readChannel);
+		xorEncryption.setOutputChannel(writeChannel);
+		char[] password = {'a','b'};
+		xorEncryption.getXorAddition().makeHashArrayFromPassword(password);
     }
+    
+	@Test
+	public void checkSimpleXorEncoding() throws Exception{
+		/*preprare*/
+		int size = 64;
+		byte[] in = new byte[size];
+		new Random().nextBytes(in);
+		writeChannel.setSize(size);
+		writeChannel.reset();
+		
+		/*action*/
+		readChannel.setSendMessage(in);
+		xorEncryption.run();
+		readChannel.setSendMessage(writeChannel.getGotMessage());
+		writeChannel.reset();
+		xorEncryption.run();
+		
+		/*assertion*/
+		int bytesRead = writeChannel.getGotLength();
+		assertEquals(size, bytesRead);
+		assertArrayEquals(in, writeChannel.getGotMessage());
+	}
+	
+	@Test
+	public void checkMultiBufferXorEncoding() throws Exception{
+		/*preprare*/
+		int size = 1500;
+		byte[] in = new byte[size];
+		new Random().nextBytes(in);
+		writeChannel.setSize(size);
+		writeChannel.reset();
+		
+		/*action*/
+		readChannel.setSendMessage(in);
+		xorEncryption.run();
+		readChannel.setSendMessage(writeChannel.getGotMessage());
+		writeChannel.reset();
+		xorEncryption.run();
+		
+		/*assertion*/
+		int bytesRead = writeChannel.getGotLength();
+		assertEquals(size, bytesRead);
+		assertArrayEquals(in, writeChannel.getGotMessage());
+	}
 
     @After
     public void tearDown() throws Exception {
