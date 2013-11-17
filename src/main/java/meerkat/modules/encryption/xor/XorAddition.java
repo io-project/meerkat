6f42c256f274ea84bbd86d1874a6e435382acc25
Karol <karol.banys@gmail.com>
2013-11-17 17:40:49 +0100

    Add class XorAddition.java, which is responsible for codding password and make form int hashCode array.

---
+++ src/main/java/meerkat/modules/encryption/xor/XorAddition.java
@@ -0,0 +1,49 @@
+package meerkat.modules.encryption.xor;
+
+import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class XorAddition {
+	
+	private byte[] hashCode = null;
+	
+	public byte[] getHashCodeArray(){
+		return hashCode;
+	}
+	
+	/**
+	 * This method make password coded by sha512. 
+	 * @param byte[] that represent password
+	 * @return byte[] that represent this password as hash sha512
+	 */
+	public static byte[] makeByteArrayFromPassword(byte[] password){
+		return  DigestUtils.sha( password );
+	}
+	
+	/**
+	 * This method encode char[] password to byte[] password
+	 * @param char[] that represent password obtain from IDialog
+	 * @return byte[] that represent this password encoded to byte array
+	 */
+	public static byte[] makePasswordEncodeCharToByteArray(char[] password) throws CharacterCodingException{
+		Charset charset = Charset.forName("UTF-8");
+		CharsetEncoder encoder = charset.newEncoder();
+		CharBuffer charBuffer = CharBuffer.wrap(password);
+		return encoder.encode(charBuffer).array();
+	}
+	
+	public boolean makeHashArrayFromPassword(char[] password){
+		try{
+			byte[] encodedPassword = makePasswordEncodeCharToByteArray(password);
+			hashCode = makeByteArrayFromPassword(encodedPassword);
+		} catch (CharacterCodingException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+}
