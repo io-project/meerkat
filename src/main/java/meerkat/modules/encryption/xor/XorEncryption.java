f38a3c07479bcbe23255c4a1e13e24e4a1042041
Karol <karol.banys@gmail.com>
2013-11-20 10:09:55 +0100

    Add validators and remove warning.

--- src/main/java/meerkat/modules/encryption/xor/XorEncryption.java
+++ src/main/java/meerkat/modules/encryption/xor/XorEncryption.java
@@ -4,6 +4,7 @@ import meerkat.modules.encryption.IEncryptionImplementation;
 import meerkat.modules.gui.IDialog;
 import meerkat.modules.gui.IDialogBuilder;
 import meerkat.modules.gui.IDialogBuilderFactory;
+import meerkat.modules.gui.IPasswordValidator;
 
 import java.nio.ByteBuffer;
 import java.nio.channels.InterruptibleChannel;
@@ -24,12 +25,18 @@ public class XorEncryption implements IEncryptionImplementation {
 
     @Override
     public boolean prepare(IDialogBuilderFactory<?> dialogBuilderFactory) {
-        IDialogBuilder dialogBuilder = dialogBuilderFactory.newDialogBuilder();
+        IDialogBuilder<?> dialogBuilder = dialogBuilderFactory.newDialogBuilder();
         String label_password = "Haslo";
-        String label_description = "Podaj haslo chroniace kodowanie:";
+        String label_description = "Podaj haslo chroniace kodowanie (1-8 znakow):";
+        IPasswordValidator passwordValidator = new IPasswordValidator(){
+        	@Override
+        	public boolean validate(String label, char[] value){
+        		return value != null && value.length > 0 && value.length < 9;
+        	}
+        };
         IDialog dialog = dialogBuilder
                 .addLabel(label_description)
-                .addPasswordEdit(label_password)
+                .addPasswordEdit(label_password, passwordValidator)
                 .build();
         if (dialog.exec()) {
             return xorAddition.makeHashArrayFromPassword(dialog.getPasswordValue(label_password));
