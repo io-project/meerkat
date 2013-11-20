a116aca9a861fdb2de424ee96c740deb17f7a3da
tomek <nocekt@gmail.com>
2013-11-20 11:17:13 +0100

    Single file serialization plugin update - added validators.

--- src/main/java/meerkat/modules/serialization/singleFile/SingleFileSerializationImplementation.java
+++ src/main/java/meerkat/modules/serialization/singleFile/SingleFileSerializationImplementation.java
@@ -1,5 +1,6 @@
 package meerkat.modules.serialization.singleFile;
 
+import java.io.File;
 import meerkat.modules.gui.IDialog;
 import meerkat.modules.gui.IDialogBuilderFactory;
 import meerkat.modules.serialization.ISerializationImplementation;
@@ -9,6 +10,7 @@ import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.InterruptibleChannel;
 import java.nio.channels.WritableByteChannel;
+import meerkat.modules.gui.IFileValidator;
 
 /**
  * @author Tomasz Nocek
@@ -16,7 +18,7 @@ import java.nio.channels.WritableByteChannel;
 public class SingleFileSerializationImplementation implements ISerializationImplementation {
 
     private WritableByteChannel outputChannel = null;
-    private String filePath = null;
+    private File inputFile = null;
 
     @Override
     public <T extends WritableByteChannel & InterruptibleChannel> void setOutputChannel(T channel) {
@@ -25,20 +27,29 @@ public class SingleFileSerializationImplementation implements ISerializationImpl
 
     @Override
     public boolean prepare(IDialogBuilderFactory<?> dialogBuilderFactory) {
-        IDialog d = dialogBuilderFactory.newDialogBuilder().addFileChooser("Podaj ścieżkę pliku:").build();
-        if (d.exec()) {
-            filePath = d.getFileValue("Podaj ścieżkę pliku:").getAbsolutePath();
-            return true;
+        
+        IFileValidator v = new IFileValidator() {
+            @Override
+            public boolean validate(String label, File value) {
+                return  value.exists() && value.isFile() && value.canRead();
+            }
+        };
+        
+        IDialog d = dialogBuilderFactory.newDialogBuilder().addFileChooser("Podaj ścieżkę pliku:",v).build();
+        while(d.exec()) {
+            if(d.validate()) {
+                inputFile = d.getFileValue("Podaj ścieżkę pliku:");
+                return true;
+            }
         }
         return false;
     }
 
     @Override
     public void run() throws Exception {
-        if (filePath == null || outputChannel == null) throw new NullPointerException();
-        // throws FileNotFoundException or Permission denied
-        RandomAccessFile file = new RandomAccessFile(filePath, "r");
+        if(inputFile == null || outputChannel == null) throw new NullPointerException();
 
+        RandomAccessFile file = new RandomAccessFile(inputFile, "r");
         FileChannel fileChannel = file.getChannel();
         ByteBuffer buffer = ByteBuffer.allocate(32768);
         int bytesRead = fileChannel.read(buffer);
