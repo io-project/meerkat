5fea9068962bf5e1648998303a11d092b3457c31
Karol <karol.banys@gmail.com>
2013-11-22 11:22:02 +0100

    Refactoring and remove warnings from DES ENCRYPTION.

--- src/main/java/meerkat/modules/encryption/des/DesAddition.java
+++ src/main/java/meerkat/modules/encryption/des/DesAddition.java
@@ -1,20 +1,12 @@
 package meerkat.modules.encryption.des;
 
-import java.nio.CharBuffer;
-import java.nio.charset.CharacterCodingException;
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetEncoder;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.DESKeySpec;
-import javax.crypto.spec.SecretKeySpec;
-
-import org.apache.commons.codec.digest.DigestUtils;
 
 public class DesAddition {
 
@@ -29,12 +21,14 @@ public class DesAddition {
 	 * 
 	 * @param byte[] that represent password
 	 * @return SecretKey that represent this password as Key
-	 * @throws InvalidKeyException 
+	 * @throws InvalidKeyException
 	 * @throws NoSuchAlgorithmException
-	 * @throws InvalidKeySpecException 
+	 * @throws InvalidKeySpecException
 	 */
-	
-	public static SecretKey makeKeyFromPassword(byte[] password) throws InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException {
+
+	public static SecretKey makeKeyFromPassword(byte[] password)
+			throws InvalidKeyException, NoSuchAlgorithmException,
+			InvalidKeySpecException {
 		DESKeySpec dks = new DESKeySpec(password);
 		SecretKeyFactory skf = SecretKeyFactory.getInstance("DES");
 		return skf.generateSecret(dks);
