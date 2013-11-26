3e35df5af4389c5fb6ea8c02f78c74c4906a4d3e
tomek <nocekt@gmail.com>
2013-11-26 17:08:41 +0100

    Gui update - blocking file chooser popup.

--- src/main/java/meerkat/modules/gui/standard/FileChooserPopup.java
+++ src/main/java/meerkat/modules/gui/standard/FileChooserPopup.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
+import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JTextField;
 import javax.swing.JFrame;
@@ -12,17 +13,19 @@ import javax.swing.JFrame;
  *
  * @author Tomasz Nocek
  */
-public class FileChoserPopup {
+public class FileChooserPopup {
     
     private final JTextField jFTF;
     
-    public FileChoserPopup(JTextField jFTF) {
+    public FileChooserPopup(JTextField jFTF) {
         this.jFTF = jFTF;
     }
     
     public void build(boolean enableFiles) {
-        final JFrame frame = new JFrame("FileChooser");
-        final Container contentPane = frame.getContentPane();
+        
+        final JFrame frame = new JFrame("File chooser");
+        final JDialog dialog = new JDialog(frame,true);
+        final Container contentPane = dialog.getContentPane();
 
         final JFileChooser fileChooser = new JFileChooser(".");
         fileChooser.setControlButtonsAreShown(true);
@@ -35,14 +38,14 @@ public class FileChoserPopup {
             public void actionPerformed(ActionEvent evt) {
                 if(JFileChooser.APPROVE_SELECTION.equals(evt.getActionCommand())) {
                     jFTF.setText(fileChooser.getSelectedFile().getPath());
-                    frame.setVisible(false);
+                    dialog.setVisible(false);
                 } else if (JFileChooser.CANCEL_SELECTION.equals(evt.getActionCommand())) {
-                    frame.setVisible(false);
+                    dialog.setVisible(false);
                 }
             }
         });
         
-        frame.pack();
-        frame.setVisible(true);
+        dialog.pack();
+        dialog.setVisible(true);
     }
 }
